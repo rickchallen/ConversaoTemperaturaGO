633bc42a36dd3803b64c7559ede0f19ee74ccd7b
rickchallen <wrf201@gmail.com>
2025-03-22 20:34:39 -0300

    insercao dos arquivos

---
+++ conversao.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const ebulicaok float64 = 373.15
+
+func main() {
+
+	var ebulicaoC float64 = ebulicaok - 273.15
+	fmt.Println(" O ponto De Ebulição de kelvin Para Celsius é  ", ebulicaoC, "°C")
+
+}
